792430cffe52eb60acedd7bdd32a0aef2bace5cb
ahmedrazek <ahmedrazek97@gmail.com>
2024-07-20 16:42:57 +0300

    fix bug

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -17,6 +17,7 @@ export async function POST(request: Request) {
       email: user?.email || "",
       uuid: user?.id || "",
     });
+    //@ts-ignore
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       billing_address_collection: "required",
